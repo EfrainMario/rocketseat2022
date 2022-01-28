19434debff23e0516ede8cf6e4c3eab273dfa680
EfrainMario <efrainmario345@gmail.com>
2022-01-28 08:39:28 +0100

    Avatar fix 2

--- src/components/Home/Avatar.tsx
+++ src/components/Home/Avatar.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Avatar, {AvatarProps} from '@mui/material/Avatar';
-import { SxProps } from '@mui/material';
 
 export default function MyAvatar(props:AvatarProps) {
     return (
