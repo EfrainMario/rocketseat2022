82cc5e88d4a5190f1dfc0d5d4877df94cca87630
EfrainMario <efrainmario345@gmail.com>
2022-01-28 08:33:02 +0100

    Avatar  fix

--- src/components/Home/Avatar.tsx
+++ src/components/Home/Avatar.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import Avatar from '@mui/material/Avatar';
+import Avatar, {AvatarProps} from '@mui/material/Avatar';
+import { SxProps } from '@mui/material';
 
-export default function MyAvatar(props) {
+export default function MyAvatar(props:AvatarProps) {
     return (
         <Avatar {...props} sx={{border: '6px solid #fff', width: 128, height:128, ...props.sx}}/>
     )
